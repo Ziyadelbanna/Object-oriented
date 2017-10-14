ddc724c00d758bcbf9dcc8dd74ff1471266fe762
Ziyad El Banna <Ziyad El Banna@DESKTOP-9F8KNO6>
2017-10-14 21:56:17 +0200

    commit

--- src/eg/edu/alexu/csd/oop/calculator/cs27/CalculatorUnit.java
+++ src/eg/edu/alexu/csd/oop/calculator/cs27/CalculatorUnit.java
@@ -155,7 +155,7 @@ public class CalculatorUnit implements Calculator {
 			for (int i = 0; i < operationArr.size(); i++) {
 				save.writeObject(operationArr.get(i));
 			}
-			slcount = operationArr.size();
+			slcount = current;
 
 			// closes save
 			save.close();
