25486e14a558cb5eb3c9c0c5c15d9e5a26b556a1
Ziyad El Banna <Ziyad El Banna@DESKTOP-9F8KNO6>
2017-10-27 20:29:18 +0200

    commit

--- src/eg/edu/alexu/csd/oop/draw/cs27/DrawEngine.java
+++ src/eg/edu/alexu/csd/oop/draw/cs27/DrawEngine.java
@@ -33,16 +33,16 @@ public class DrawEngine implements DrawingEngine {
 
 	public void refresh(Graphics canvas) {
 
-		if (canvas == null) {
-			throw null;
-		} else {
-			g2 = (Graphics2D) canvas;
-			for (Shape l : shapeslists.get(currentindex)) {
-				l.draw(g2);
-			}
+		// if (canvas == null) {
+		/// throw null;
+		// } else {
+		// g2 = (Graphics2D) canvas;
+		for (Shape l : shapeslists.get(currentindex)) {
+			l.draw(canvas);
 		}
-
 	}
+
+	// }
 
 	public void addShape(Shape shape) {
 		if (currentindex < shapeslists.size() - 1) {
