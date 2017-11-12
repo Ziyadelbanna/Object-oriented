b400c5131558bd4b73b8f4ca62c6953f7e1cad8f
Ziyad El Banna <Ziyad El Banna@DESKTOP-9F8KNO6>
2017-11-12 22:43:52 +0200

    commit

--- src/eg/edu/alexu/csd/oop/draw/cs27/DrawEngine.java
+++ src/eg/edu/alexu/csd/oop/draw/cs27/DrawEngine.java
@@ -68,7 +68,7 @@ public class DrawEngine implements DrawingEngine {
 			shapes.add(shape);
 			shapeslists.add(new LinkedList<Shape>(shapes));
 		}
-		if (shapeslists.size() > 21) {
+		if (shapeslists.size() > 20) {
 			shapeslists.remove(0);
 		}
 		redoo = false;
@@ -99,7 +99,7 @@ public class DrawEngine implements DrawingEngine {
 			}
 		}
 		shapeslists.add(new LinkedList<Shape>(newshapes));
-		if (shapeslists.size() > 21) {
+		if (shapeslists.size() > 20) {
 			shapeslists.remove(0);
 		}
 		currentindex = shapeslists.size() - 1;
@@ -132,7 +132,7 @@ public class DrawEngine implements DrawingEngine {
 			}
 		}
 		shapeslists.add(new LinkedList<Shape>(newshapes));
-		if (shapeslists.size() > 21) {
+		if (shapeslists.size() > 20) {
 			shapeslists.remove(0);
 		}
 		currentindex = shapeslists.size() - 1;
