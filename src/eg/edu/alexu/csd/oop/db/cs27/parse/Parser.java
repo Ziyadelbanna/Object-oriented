10f2645135989dac4848de3325bb3908f81b8dd6
Ziyad El Banna <Ziyad El Banna@DESKTOP-9F8KNO6>
2017-12-04 01:05:39 +0200

    another upd

--- src/eg/edu/alexu/csd/oop/db/cs27/parse/Parser.java
+++
@@ -1,9 +0,0 @@
-package eg.edu.alexu.csd.oop.db.cs27.parse;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public interface Parser {
-	/* public Map<String,Object> collected = new HashMap<String,Object>(); */
-	public Map<String, Object> getMap(String quary);
-}
